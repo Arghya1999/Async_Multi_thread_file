6c3d1d3f6a248e4c5e04fc594b5e71626e05dba8
Arghya Pan <panarghya2015@gmail.com>
2023-08-12 02:57:22 +0530

    Initial commit

---
+++ main/java/com/example/thread/MultiThread1Application.java
@@ -0,0 +1,13 @@
+package com.example.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThread1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiThread1Application.class, args);
+	}
+
+}
